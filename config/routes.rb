f2d6536690abf986da2f10863a03a22ae3336514
Ruli_AlSe <ra.almanzaserrano@gmail.com>
2019-09-24 11:24:48 -0500

    Add root path for authenticated users and add stylesheets and Scss

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-
+  authenticated :user do
+    root 'docs#index', as: 'authenticated_root'
+  end
   resources :docs
+
+  root 'welcome#index'
 end
